91eab7c9bafaabc50f9a87b922a132956b9dc746
NT BOTS <64780226+jobin888@users.noreply.github.com>
2022-05-28 15:41:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,11 @@
 import os
 
-from heroku3 import from_key
 from pyrogram import Client
-from pyromod import listen
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-APP_NAME = os.environ.get("APP_NAME", None)
-API_KEY = os.environ.get("API_KEY", None)
-HU_APP = from_key(API_KEY).apps()[APP_NAME]
+s1 = "YOUR SESSION STRING"
 
-bot = Client(":memory:",
-             api_id=API_ID,
-             api_hash=API_HASH,
-             bot_token=BOT_TOKEN)
+app = Client(s1,  api_id=YOU_API_ID, api_hash="YOU API HASH")
+
+app.storage.SESSION_STRING_FORMAT=">B?256sQ?"
+
+app.start()
